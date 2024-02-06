af9641e63cd25b76fb0414db6eb5f5bcbdac3504
shobika1112 <palanivel.shobika@divum.in>
2024-02-06 14:09:18 +0530

    merge check from shobika

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Switching from "./Components/Switching/Switching";
 // import NotificationSlideBar from "./Components/navbar/NotificationSlideBar";
@@ -26,40 +25,6 @@ function App() {
       {/* <SecondPage/> */}
       <AdminRouter />
       {/* <Landingpage /> */}
-=======
-import React ,{useState} from "react";
-import "./App.css";
-// import CodingTest from "./components/CodingTest";
-// import Piechart from "./components/Piechart";
-// import AdminRouter from "./routers/AdminRouter";
-import Bulkdata from "./components/Bulkdata";
-
-function App() {
-
-  const [chartData, setChartData] = useState({
-    labels: [
-      `logical: \n easy : ${20} \n medium : ${20} \n hard : ${20}`,
-      `verbal: \n easy : ${12} \n medium : ${15} \n hard : ${10}`,
-      `aptitude: \n easy : ${20} \n medium : ${30} \n hard : ${17}`
-    ],
-    datasets: [{
-      data: [30, 45, 24],
-      backgroundColor: [
-        'rgb(211, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      cutout:"50%"
-    }]
-  });
-
-  return(
-    <div>
-      {/* <CodingTest/> */}
-      <Bulkdata/>
-      
-     {/* <Piechart chartData={chartData}/> */}
->>>>>>> frontend
     </div>
   )
   // return (
@@ -69,4 +34,5 @@ function App() {
   //   </div>
   // );
 }
+
 export default App;
